92e50c3eea0b50b5224a1530cf234160153d2bac
Kat Allen <katallen@gmail.com>
2024-08-29 16:25:11 -0400

    updated collision scene for table 24" from the center of the pillar

--- poli2_launch/scripts/setup_beep_collision_scene.py
+++ poli2_launch/scripts/setup_beep_collision_scene.py
@@ -46,6 +46,7 @@ if __name__=="__main__":
     p.remove("table")
     p.remove("back")
     p.remove("roof")
+    p.remove("head")
 
     table_z = -0.04
     table_thickness = 0.05
@@ -63,10 +64,6 @@ if __name__=="__main__":
 
 
 
-    p.add_box("back", "base_link", size=(2, 0.1, 2), position=(-.35, 0.0, 1), color=(0,0,1.,0.4),orientation=(6.85288451e-01, 7.28271312e-01, -5.45713671e-04, 5.79942083e-04))
-    p.add_box("head", "base_link", size=(.28, 0.23, .2), position=(0.0, 0.06, 1.22), color=(0.7,0,1.,0.4))
-    p.add_box("table", "base_link", size=(2, 2, table_thickness), position=(1.25, 0, .74), color=(0,0,1.,0.4))
-    # p.add_box("back", "world", size=(2, 0.1, 2), position=(-.28, 0.0, 1), color=(0,0,1.,0.4),orientation=(2,2,0,0))
-    # p.add_box("table", "world", size=(2, 2, table_thickness), position=(1.25, 0, .48), color=(0,0,1.,0.4))
-    # p.add_box("back", "base_link", size=(2, 0.1, 2), position=(-.28, 0.0, 1), color=(0,0,1.,0.4),orientation=(2,2,0,0))
-    #p.add_box("roof", "base_link", size=(2, 2, table_thickness), position=(0, 0, 0.85 + table_thickness/2), color=(0, 0, 0, 0.4))
+    p.add_box("back", "base_link", size=(0.1, 2, 2), position=(-0.35, 0.0, 1), color=(0,0,1.,0.4))
+    p.add_box("head", "base_link", size=(0.28, 0.23,0.2), position=(0.0, 0.06, 1.22), color=(0.7,0.0,1.0,0.4))
+    p.add_box("table", "base_link", size=(0.6, 2, table_thickness), position=(0.9144, 0.0, .73), color=(0,0,1.0,0.4))
